d6a5c638fdb5177f99a9060c7db7b4308ee5095b
xdcgh <44773625+xdcgh@users.noreply.github.com>
2022-10-15 18:15:37 +0800

    Delete 申请人确认 - 学生离校外出申请_files directory

--- 申请人确认 - 学生离校外出申请_files/mobile(1).js
+++
@@ -1,6 +0,0 @@
-$$.ready(function(){
-	var myDate = new Date();
-    var nowYear = myDate.getFullYear();
-    var footerContent="Copyright "+nowYear+" South China Normal University. All Rights Reserved/华南师范大学 版权所有";
-    $("#div_footer_copy_content").html(footerContent);
-})
